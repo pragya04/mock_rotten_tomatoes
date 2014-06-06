b5fe90ac8bb1cb95ee1c88f4ecca3426d98cd1fa
pherwani <pherwani@yahoo-inc.com>
2014-06-05 17:10:34 -0700

    Initial Commit

---
+++ Rotten Tomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rotten Tomatoes
+//
+//  Created by Pragya  Pherwani on 6/5/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "rottenTomatoesAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([rottenTomatoesAppDelegate class]));
+    }
+}
---
+++ Rotten Tomatoes/rottenTomatoesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  rottenTomatoesAppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Pragya  Pherwani on 6/5/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rottenTomatoesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
